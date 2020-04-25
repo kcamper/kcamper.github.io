c878afc29cbde74983faf4a87326dd8bee49437f
kcamper <60638776+kcamper@users.noreply.github.com>
2020-04-25 13:02:53 -0500

    Add files via upload

---
+++ very-best/db/migrate/20161122134844_create_venues.rb
@@ -0,0 +1,12 @@
+class CreateVenues < ActiveRecord::Migration[5.0]
+  def change
+    create_table :venues do |t|
+      t.string :name
+      t.string :address
+      t.integer :neighborhood_id
+
+      t.timestamps
+
+    end
+  end
+end
